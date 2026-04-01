e81fed3035da5d513adf9ab125397b463e8184f8
muschellij2 <muschellij2@gmail.com>
2026-03-31 23:41:23 -0400

    added brainchop

---
+++ R/brainchop.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#SBATCH --job-name=BRAINCHOP
+#SBATCH --partition=gpu          # <-- only GPU workloads belong here
+#SBATCH --gres=gpu:1             # 1 GPU; change to :2, :4 … if the code scales
+#SBATCH --cpus-per-task=4        # threads available to PyTorch / nnUNet
+#SBATCH --mem=20G
+#SBATCH --time=1-00:00:00
+#SBATCH --output=eofiles/BRAINCHOP_%A.out
+#SBATCH --error=eofiles/BRAINCHOP_%A.err
+
+## INSTALL THE CONDAENV
+# module unload conda_R || true
+# module unload freesurfer || true
+# module unload fsl || true
+# module load conda
+# conda create -n brainchop_env python=3.11
+# conda activate brainchop
+# uv pip install brainchop
+
+source ~/.bash_profile
+module unload conda_R || true
+module unload freesurfer || true
+module unload fsl || true
+module load conda
+conda activate brainchop_env
+cd $cq500
+
+files=`ls data/noneck_512/*.nii.gz`
+ifile=${files}
+brain_outdir="data/noneck_brain_extracted_brainchop_512"
+mkdir -p ${brain_outdir}
+mask_outdir="data/noneck_brain_mask_brainchop_512"
+mkdir -p ${mask_outdir}
+
+
+# export CUDA_DIRECT_HOST_MEM=0
+for ifile in $files;
+do
+  echo "$ifile"
+  bn=`basename ${ifile}`
+  brain="${brain_outdir}/${bn}"
+  mask="${mask_outdir}/${bn}"
+  if [ ! -f "${brain}"  ] || [ ! -f "${mask}"  ]; then
+    echo "processing ${ifile}"
+    if [ -z "$CUDA_VISIBLE_DEVICES" ];
+    then
+      echo "using LLVM"
+      LLVM=1 brainchop ${ifile} --output ${brain} --mask ${mask} --ct --model mindgrab
+    else
+      echo "using GPU"
+      GPU=1 brainchop ${ifile} --output ${brain} --mask ${mask} --ct --model mindgrab
+    fi
+  fi
+done
+
